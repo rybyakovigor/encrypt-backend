0b53398d3641895dd4be6990294b7ebe3d91c3e8
Igor Rybyakov <rybyakovigor@gmail.com>
2022-07-04 00:29:45 +0500

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // Core
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Module
@@ -7,6 +8,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder().setTitle('Encrypt API').setVersion('0.1').build();
