b1ef21122d6ee380c189788c9d3b0f55ad189c72
Igor Rybyakov <rybyakovigor@gmail.com>
2022-07-07 01:42:05 +0500

    chore(user model): add api property

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,12 +1,15 @@
 // Core
+import { ApiProperty } from '@nestjs/swagger';
 import { prop } from '@typegoose/typegoose';
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 
 export interface UserModel extends Base {}
 export class UserModel extends TimeStamps {
+  @ApiProperty()
   @prop({ required: true, unique: true })
   email: string;
 
+  @ApiProperty()
   @prop({ required: true })
   password: string;
 }
