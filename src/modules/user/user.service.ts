5baf576da439b3453223a956dcd4b5bc42883c79
Igor Rybyakov <rybyakovigor@gmail.com>
2022-07-07 00:51:54 +0500

    chore: add error handling

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 // Core
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
 
@@ -8,6 +8,9 @@ import { UserModel } from './user.model';
 
 // Dto
 import { CreateUserDto } from './dto/create-user.dto';
+
+// Types
+import { MongoErrorCodes } from 'src/@types-space/enums/mongo-error-codes.enum';
 
 @Injectable()
 export class UserService {
@@ -26,9 +29,13 @@ export class UserService {
     try {
       const newDocument = await this.userModel.create(body);
       await newDocument.save();
-      return newDocument.email;
+      return newDocument.toJSON();
     } catch (error) {
-      throw new Error(error);
+      if (error.code === MongoErrorCodes.UniqueViolation) {
+        throw new ConflictException('User with that email already exists');
+      }
+
+      throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
