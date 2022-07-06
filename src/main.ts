856a69f4ce8c6cc184ccd410e65a91fd74ca5cdd
Igor Rybyakov <rybyakovigor@gmail.com>
2022-07-07 00:52:57 +0500

    chore: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+// Pipes
+import { ValidationPipe } from '@nestjs/common';
+
 // Module
 import { AppModule } from './app.module';
 
@@ -10,8 +13,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle('Encrypt API').setVersion('0.1').build();
   const document = SwaggerModule.createDocument(app, config);
